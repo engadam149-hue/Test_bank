9969bad3cdfea578928cee51a0b7077fe2c8f5dc
Adam_Mo7amed <engadam149@gmail.com>
2026-03-01 16:52:42 +0200

    Update data.py

--- data.py
+++ data.py
@@ -5,7 +5,8 @@
 # 1. استدعاء الملفات
 import ml_lec1
 import ml_lec2
-import arc_lec1  # 👈 استدعاء أسئلة الأركتكتشر
+import arc_lec1
+import arc_lec2  # 👈 أهي متسجلة هنا
 
 # 2. إعدادات المواد
 SUBJECTS = [
@@ -28,18 +29,16 @@ SUBJECTS = [
         "key": "arc",
         "lectures": [
             {"num": "01", "title": "Lec 1: Intro & Structure", "count": "43 سؤال", "key": "arc_lec1", "available": True},
-            {"num": "02", "title": "Lecture 2", "count": "قريباً", "key": "arc_lec2", "available": False}, # مقفولة
-            {"num": "03", "title": "Lecture 3", "count": "قريباً", "key": "arc_lec3", "available": False}, # مقفولة
+            {"num": "02", "title": "Lec 2: CPU & Buses", "count": "50 سؤال", "key": "arc_lec2", "available": True}, # 👈 الزرار بتاعها مفتوح هنا
+            {"num": "03", "title": "Lecture 3", "count": "قريباً", "key": "arc_lec3", "available": False}, # لسه مقفولة
         ]
     }
 ]
 
 # 3. ربط المفاتيح بملفات الأسئلة
 QUESTIONS_DB = {
-    # 🤖 أسئلة الماشين ليرننج
     "ml_lec1": ml_lec1.QUESTIONS,
     "ml_lec2": ml_lec2.QUESTIONS,
-    
-    # 💻 أسئلة الأركتكتشر
     "arc_lec1": arc_lec1.QUESTIONS, 
+    "arc_lec2": arc_lec2.QUESTIONS, # 👈 مربوطة بالأسئلة بتاعتها هنا
 }
