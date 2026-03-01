6a8cc4ea99a6c849b8aef5b9edfc7b27bcdc41a6
Adam_Mo7amed <engadam149@gmail.com>
2026-03-01 16:30:22 +0200

    Update data.py

--- data.py
+++ data.py
@@ -2,11 +2,12 @@
 # ملف: data.py
 # ==========================================
 
-# 1. استدعاء الأسئلة من الملفات اللي جنبك في نفس الفولدر
+# 1. استدعاء الملفات
 import ml_lec1
 import ml_lec2
+import arc_lec1  # 👈 استدعاء أسئلة الأركتكتشر
 
-# 2. إعدادات المادة (اللي بتظهر كزراير في الموقع)
+# 2. إعدادات المواد
 SUBJECTS = [
     {
         "icon": "🤖", 
@@ -18,11 +19,27 @@ SUBJECTS = [
             {"num": "01", "title": "Intro to ML", "count": "50 سؤال", "key": "ml_lec1", "available": True},
             {"num": "02", "title": "KNN Algorithm", "count": "50 سؤال", "key": "ml_lec2", "available": True},
         ]
+    },
+    {
+        "icon": "💻", 
+        "name": "Computer Architecture", 
+        "code": "CSE132",
+        "desc": "CPU, Memory, Buses, Cycle...", 
+        "key": "arc",
+        "lectures": [
+            {"num": "01", "title": "Lec 1: Intro & Structure", "count": "43 سؤال", "key": "arc_lec1", "available": True},
+            {"num": "02", "title": "Lecture 2", "count": "قريباً", "key": "arc_lec2", "available": False}, # مقفولة
+            {"num": "03", "title": "Lecture 3", "count": "قريباً", "key": "arc_lec3", "available": False}, # مقفولة
+        ]
     }
 ]
 
-# 3. ربط المفاتيح بالأسئلة عشان الموقع يقراها
+# 3. ربط المفاتيح بملفات الأسئلة
 QUESTIONS_DB = {
+    # 🤖 أسئلة الماشين ليرننج
     "ml_lec1": ml_lec1.QUESTIONS,
     "ml_lec2": ml_lec2.QUESTIONS,
+    
+    # 💻 أسئلة الأركتكتشر
+    "arc_lec1": arc_lec1.QUESTIONS, 
 }
